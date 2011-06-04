c76ac5ab3eb12fe319e79cce610ee687b18dd293
dbon <denjelblogs@gmail.com>
2011-06-04 11:32:34 +0200

    added batch file

---
+++ NetConnect.bat
@@ -0,0 +1,5 @@
+@echo off
+if not exist M: net use M: \\Server\Ordner1$
+if not exist Z: net use Z: \\Server\Ordner2$
+if not exist X: net use X: \\Server\Ordner3$
+if not exist Y: net use Y: \\Server\Ordner4$
